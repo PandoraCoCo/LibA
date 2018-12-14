e708d6f7323eb26d88f7224852a6613ce3b7dd7f
xush <xush@133.cn>
2018-12-14 11:40:28 +0800

    - 修改测试

---
+++ src/main/java/com/practive/AppA.java
@@ -0,0 +1,13 @@
+package com.practive;
+
+/**
+ * TODO
+ *
+ * @author XuSh
+ * @date 2018-12-14
+ */
+public class AppA {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
