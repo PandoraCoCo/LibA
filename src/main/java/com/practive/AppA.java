43b559f34602e86546994994cb1ac77bdae20aa6
xush <xush@133.cn>
2019-05-31 11:52:06 +0800

    commit 2

--- src/main/java/com/practive/AppA.java
+++ src/main/java/com/practive/AppA.java
@@ -10,5 +10,6 @@ public class AppA {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println("Test 1 commit");
+        System.out.println("Test 2 commit");
     }
 }
